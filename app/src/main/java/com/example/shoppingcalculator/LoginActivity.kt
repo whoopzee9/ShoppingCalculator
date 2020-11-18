de40fe875dd00d9b16e0c55069604eb8e4e94fc4
Bublyaev Aleksey <leshasochi2014@gmail.com>
2020-11-18 22:06:06 +0300

    Added VK auth

--- app/src/main/java/com/example/shoppingcalculator/LoginActivity.kt
+++ app/src/main/java/com/example/shoppingcalculator/LoginActivity.kt
@@ -4,7 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKApiConfig
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
+import com.vk.api.sdk.utils.VKUtils
 
 class LoginActivity: AppCompatActivity() {
     private var etLogin: EditText? = null;
@@ -16,6 +23,22 @@ class LoginActivity: AppCompatActivity() {
 
         etLogin = findViewById(R.id.et_login)
         etPassword = findViewById(R.id.et_password)
+
+        /*var fing = VKUtils.getCertificateFingerprint(this, this.packageName)
+        if (fing != null) {
+            for (item in fing) {
+                println(item)
+            }
+        }*/
+        if (VK.isLoggedIn()) {
+            println(VK.getUserId())
+            val intent = Intent(applicationContext, MainActivity::class.java)
+
+            //intent.putExtra("token", token.accessToken)
+            intent.putExtra("username", etLogin!!.text.toString().trim())
+            startActivity(intent)
+            finish()
+        }
     }
 
     fun onRegisterClick(view: View) {
@@ -26,6 +49,34 @@ class LoginActivity: AppCompatActivity() {
         startMainActivityAndClose("")
     }
 
+    fun onLoginViaVKClick(view: View) {
+        VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object: VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+
+                intent.putExtra("token", token.accessToken)
+                intent.putExtra("username", etLogin!!.text.toString().trim())
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                val toast = Toast.makeText(
+                    applicationContext,
+                    "Auth failed!", Toast.LENGTH_LONG
+                )
+                toast.show()
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
     private fun startMainActivityAndClose(result: String) {
         val intent = Intent(this, MainActivity::class.java)
 
@@ -34,4 +85,5 @@ class LoginActivity: AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
 }
