21e284ab27faaa56f643c7886196cbf2b0d84183
Tolstikov Grigoriy <tolstikov.gn@edu.spbstu.ru>
2020-12-17 03:12:18 +0300

    тесты и код рефактор

--- app/src/main/java/com/example/shoppingcalculator/Utils.kt
+++ app/src/main/java/com/example/shoppingcalculator/Utils.kt
@@ -1,20 +1,30 @@
 package com.example.shoppingcalculator
 
-
-    fun getFormattedTime(hour: Int, minute: Int): String {
-        if ((hour >= 0) and (hour < 24) and (minute < 60) and (minute >= 0)) {
-            val hourStr: String = if (hour < 10) {
-                "0$hour"
-            } else {
-                hour.toString()
-            }
-            var minuteStr: String = if (minute < 10) {
-                "0$minute"
-            } else {
-                minute.toString()
-            }
-            return "$hourStr:$minuteStr"
+fun getFormattedTime(hour: Int, minute: Int): String {
+    if ((hour >= 0) and (hour < 24) and (minute < 60) and (minute >= 0)) {
+        val hourStr: String = if (hour < 10) {
+            "0$hour"
         } else {
-            return "00:00"
+            hour.toString()
+        }
+        var minuteStr: String = if (minute < 10) {
+            "0$minute"
+        } else {
+            minute.toString()
+        }
+        return "$hourStr:$minuteStr"
+    } else {
+        return "00:00"
+    }
+}
+
+
+fun debtCounting(users: List<PaymentUser>): Double {
+    var total = 0.0
+    for (item in users) {
+        if (!item.isPaid) {
+            total += item.payment
         }
     }
+    return total
+}
